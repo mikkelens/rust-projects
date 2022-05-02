a5e40e68236f43e2695b96123fe4c43367e3b808
Mikkel S <Mikkel123sej@gmail.com>
2022-05-02 13:45:31 +0200

    another project (branches)

---
+++ branches/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 7;
+
+    if number < 5 {
+        println!("Condition was true!");
+    } else {
+        println!("Condition was false!");
+    }
+}
--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,9 @@
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = plus_one(5);
+
+    println!("The value of y is: {}", x);
 }
