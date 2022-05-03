1a6d5ac279d500bc1849393253b045508d5432a9
Mikkel S <Mikkel123sej@gmail.com>
2022-05-03 15:44:11 +0200

    finished practice_lyrics (christmas carol)

--- chapter_3/practice_lyrics/src/main.rs
+++ chapter_3/practice_lyrics/src/main.rs
@@ -1,3 +1,25 @@
+use crate::strings::{DATE_NAMES, NEW_DAY_GIFTS};
+
+mod strings;
+
 fn main() {
-    println!("Hello, world!");
+    for day in 0..DATE_NAMES.len() { // counting up
+        println!("\nOn the {} day of Christmas,", DATE_NAMES[day]);
+        println!("My good friends brought to me:");
+
+        for lyric in (0..=day).rev() { // counting down
+            if day == 0 {
+                // first gift alone
+                println!("{}.", NEW_DAY_GIFTS[lyric])
+            } else {
+                if lyric > 0 {
+                    // most days
+                    println!("{},", NEW_DAY_GIFTS[lyric]);
+                } else {
+                    // first gift as last sentence
+                    println!("And {}.", NEW_DAY_GIFTS[lyric]);
+                }
+            }
+        }
+    }
 }
---
+++ chapter_3/practice_lyrics/src/strings.rs
@@ -0,0 +1,29 @@
+pub const DATE_NAMES: [&str; 12] = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "11th",
+    "12th"
+];
+
+pub const NEW_DAY_GIFTS: [&str; DATE_NAMES.len()] = [
+    "A song and a Christmas tree",
+    "Two candy canes",
+    "Three boughs of holly",
+    "Four colored lights",
+    "A shining star",
+    "Little silver bells",
+    "Candles a-glowing",
+    "Gold and silver tinsel",
+    "A guardian angel",
+    "Some mistletoe",
+    "Gifts for one and all",
+    "All their good wishes"
+];
