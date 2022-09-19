fe981c395053bf0b034ce1d6ba0f8568510cc752
Mikkel <Mikkel123sej@gmail.com>
2022-09-19 13:03:46 +0200

    added things to minigrep

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,6 +1,40 @@
-use std::env;
+extern crate core;
+
+// mod lib;
+
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Invalid file path?");
+
+    println!("Text content: \n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Error: Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
