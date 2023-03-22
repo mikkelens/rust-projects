12d8d20f4005807c1b662035f2e591c4332659c8
Mikkel <Mikkel123sej@gmail.com>
2023-03-22 14:58:35 +0100

    changes

--- chapter_12/minigrep/src/lib.rs
+++ chapter_12/minigrep/src/lib.rs
@@ -1,55 +1,51 @@
 use std::error::Error;
 use std::{fs, env};
 
+// tests //
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn case_sensitive() {
-        let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
 Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+        assert_eq!(vec!["safe, fast, productive."], search("duct", contents))
     }
 
     #[test]
     fn case_insensitive() {
-        let query = "rUsT";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
-
         assert_eq!(
             vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
+            search_case_insensitive("rUsT", contents)
         );
     }
 
     #[test]
     fn one_result() {
-        let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search("duct", contents));
     }
 }
 
+// code //
 pub struct Config {
     pub query: String,
     pub file_path: String,
     pub ignore_case: bool,
 }
-
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
