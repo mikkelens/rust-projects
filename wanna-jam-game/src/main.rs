83473218bdae8682ab86dcec051a6b5ff99c58cf
Mikkel <Mikkel123sej@gmail.com>
2023-08-08 21:32:29 +0200

    made initial app

--- wanna-jam-game/src/main.rs
+++ wanna-jam-game/src/main.rs
@@ -1,3 +1,28 @@
-use bevy::prelude::App;
+#![allow(clippy::needless_pass_by_value)] // disables unhelpful Query reference lint
 
-fn main() { App::new().run(); }
+use bevy::prelude::*;
+
+fn main() {
+	App::new()
+		.add_plugins(DefaultPlugins)
+		.add_systems(Startup, add_people)
+		.add_systems(Update, greet_people)
+		.run();
+}
+
+#[derive(Component)]
+struct Person;
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+	commands.spawn((Person, Name("Elaina Proctor".into())));
+	commands.spawn((Person, Name("Renzo Hume".into())));
+	commands.spawn((Person, Name("Zayna Nieves".into())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+	for name in &query {
+		println!("hello {}!", name.0);
+	}
+}
