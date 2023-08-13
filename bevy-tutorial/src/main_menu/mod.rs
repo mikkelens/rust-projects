73a0e3069115ee972ec5335adf912cc0357c0bec
Mikkel <Mikkel123sej@gmail.com>
2023-08-13 22:20:30 +0200

    cant work anymore

--- bevy-tutorial/src/main_menu/mod.rs
+++ bevy-tutorial/src/main_menu/mod.rs
@@ -1,10 +1,20 @@
 use bevy::prelude::*;
 
+mod components;
+mod styles;
+mod systems;
+
+use systems::layout::*;
+
+use crate::AppState;
+
 pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
-	fn build(&self, app: &mut App) { app.add_systems(Startup, main_menu); }
-}
-
-fn main_menu() {
-	println!("You are on the main menu.");
+	fn build(&self, app: &mut App) {
+		app
+			// enter
+			.add_systems(OnEnter(AppState::MainMenu), spawn_main_menu)
+			// exit
+			.add_systems(OnExit(AppState::MainMenu), despawn_main_menu);
+	}
 }
