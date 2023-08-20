d96828eaac7d679e0565d1b182025daee44c6302
Mikkel <Mikkel123sej@gmail.com>
2023-08-20 04:12:16 +0200

    work on minesweeper thing

--- minesweeper-thing/src/main.rs
+++ minesweeper-thing/src/main.rs
@@ -1,14 +1,22 @@
+#![allow(clippy::needless_pass_by_value, clippy::module_name_repetitions)]
+
 mod game;
 mod states;
 
 use bevy::prelude::*;
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use game::GamePlugin;
 
-use self::{game::*, states::*};
+// use self::{game::*, states::*};
 
 fn main() {
-	App::new()
-		.add_state::<AppState>()
-		.add_plugins(DefaultPlugins)
+	let mut app = App::new();
+
+	#[cfg(feature = "debug")] // debug hierachy inspector
+	app.add_plugins(WorldInspectorPlugin::new());
+
+	app.add_plugins(DefaultPlugins)
 		.add_plugins(GamePlugin)
 		.run();
 }
