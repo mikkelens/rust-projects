6743e2ec263f709622041791db1fd521f1eb7496
Mikkel <Mikkel123sej@gmail.com>
2023-08-20 04:20:23 +0200

    cleaned up

--- minesweeper-thing/src/game/resources.rs
+++
@@ -1,4 +0,0 @@
-use bevy::{prelude::*, time::Timer};
-
-#[derive(Resource)]
-pub struct LevelTimer(pub Timer);
--- minesweeper-thing/src/main.rs
+++ minesweeper-thing/src/main.rs
@@ -1,22 +1,2 @@
 #![allow(clippy::needless_pass_by_value, clippy::module_name_repetitions)]
 
-mod game;
-mod states;
-
-use bevy::prelude::*;
-#[cfg(feature = "debug")]
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use game::GamePlugin;
-
-// use self::{game::*, states::*};
-
-fn main() {
-	let mut app = App::new();
-
-	#[cfg(feature = "debug")] // debug hierachy inspector
-	app.add_plugins(WorldInspectorPlugin::new());
-
-	app.add_plugins(DefaultPlugins)
-		.add_plugins(GamePlugin)
-		.run();
-}
