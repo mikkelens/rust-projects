d25f6459ab3f5d8914daf994490a9e68651440a6
Mikkel <Mikkel123sej@gmail.com>
2023-12-20 05:32:45 +0100

    lol

--- bevy-jam-4/src/main.rs
+++ bevy-jam-4/src/main.rs
@@ -1,4 +1,12 @@
+use std::ops::RangeInclusive;
 use bevy::prelude::*;
+
+struct Rect<T> {
+    width: RangeInclusive<T>,
+    height: RangeInclusive<T>
+}
+
+const CANVAS_SIZE: Rect<f32> = Rect { width: 0.0..=300.0, height: 0.0..=200.0 };
 
 fn main() {
     App::new().add_plugins(DefaultPlugins).run();
