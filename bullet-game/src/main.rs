640ec86af65ec45ef748cfcd6c4721f056f388e3
Mikkel <Mikkel123sej@gmail.com>
2024-01-16 22:12:06 +0100

    made some assets and made dynamic linking work in rustrover

--- bullet-game/src/main.rs
+++ bullet-game/src/main.rs
@@ -1,5 +1,42 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup, spawn_single_flyer))
+        .add_systems(FixedUpdate, move_flying)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+#[derive(Component)]
+struct Flying {
+    horizontal_speed: f32,
+    position: Vec2,
+}
+
+#[derive(Component)]
+struct Hittable;
+
+#[derive(Component)]
+struct Hitting;
+
+fn spawn_single_flyer(mut commands: Commands) {
+    commands.spawn((
+        Flying {
+            horizontal_speed: 5f32,
+            position: Vec2 { x: 10f32, y: 50f32 },
+        },
+        Hittable,
+    ));
+}
+
+fn move_flying(mut query: Query<&mut Flying>) {
+    for mut flyer in &mut query {
+        flyer.position.x += flyer.horizontal_speed;
+        dbg!(flyer.position);
+    }
 }
