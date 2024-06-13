e349df03066805b4e1c68444c0d4c7055ee73375
Mikkel <Mikkel123sej@gmail.com>
2024-06-13 20:39:05 +0200

    added cordic & stuff

--- rp-pico-setup/src/main.rs
+++ rp-pico-setup/src/main.rs
@@ -3,77 +3,87 @@
 #![feature(type_alias_impl_trait)]
 #![feature(iter_repeat_n)]
 
+use core::fmt::Debug;
+
 use embassy_executor::Spawner;
+use embassy_rp::pwm::Config;
 use embassy_rp::{
-	pwm,
-	pwm::{Channel, Pwm},
-	Peripheral
+    pwm::{Channel, Pwm},
+    Peripheral,
 };
 use embassy_time::{Duration, Timer};
+
 // necessary to be able to build: rtt for linking and required panic handler...
 #[allow(unused_imports)] // ...even though we don't use anything from them
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-	defmt::info!("Initializing...");
-	let mut peripherals = embassy_rp::init(Default::default());
+    defmt::info!("Initializing...");
+    let mut peripherals = embassy_rp::init(Default::default());
 
-	let mut config: pwm::Config = Default::default();
-	config.top = u16::MAX / 2;
-	config.compare_a = 8;
+    let mut led = RGBLed {
+        r: Pwm::new_output_a(
+            unsafe { peripherals.PWM_CH1.clone_unchecked() },
+            peripherals.PIN_18,
+            Config::default(),
+        ),
+        g: Pwm::new_output_b(peripherals.PWM_CH1, peripherals.PIN_19, Config::default()),
+        b: Pwm::new_output_a(peripherals.PWM_CH2, peripherals.PIN_20, Config::default()),
+    };
+    defmt::info!("Initialized.");
 
-	let mut led = RGBLed {
-		r: Pwm::new_output_a(
-			unsafe { peripherals.PWM_CH1.clone_unchecked() },
-			peripherals.PIN_18,
-			config.clone()
-		),
-		g: Pwm::new_output_b(peripherals.PWM_CH1, peripherals.PIN_19, config.clone()),
-		b: Pwm::new_output_a(peripherals.PWM_CH2, peripherals.PIN_20, config.clone())
-	};
-	defmt::info!("Initialized.");
-
-	led.cycle_counter().await;
-	unreachable!("cyclic iter")
+    led.cycle_counter().await;
+    unreachable!("cyclic iter")
 }
 
-const PWM_MAX: u16 = u16::MAX / 2;
+const PWM_MAX: u16 = u16::MAX;
 
 struct RGBLed<'d, R: Channel, G: Channel, B: Channel> {
-	r: Pwm<'d, R>,
-	g: Pwm<'d, G>,
-	b: Pwm<'d, B>
+    r: Pwm<'d, R>,
+    g: Pwm<'d, G>,
+    b: Pwm<'d, B>,
 }
 impl<'d, R: Channel, G: Channel, B: Channel> RGBLed<'d, R, G, B> {
-	fn set_counter_max(&mut self) {
-		self.r.set_counter(PWM_MAX);
-		self.g.set_counter(PWM_MAX);
-		self.b.set_counter(PWM_MAX);
-	}
+    fn set_counter_max(&mut self) {
+        self.r.set_counter(PWM_MAX);
+        self.g.set_counter(PWM_MAX);
+        self.b.set_counter(PWM_MAX);
+    }
 
-	#[allow(unused)]
-	async fn cycle_counter(&mut self) {
-		let up_fade = 0..=PWM_MAX;
-		let r_iter = up_fade
-			.clone()
-			.chain(core::iter::repeat_n(*up_fade.end(), up_fade.len()))
-			.chain(up_fade.clone().rev())
-			.chain(core::iter::repeat_n(*up_fade.start(), up_fade.len()));
-		let g_iter = r_iter.clone().skip(up_fade.len());
-		let b_iter = g_iter.clone().skip(up_fade.len());
-		for (r_lvl, (g_lvl, b_lvl)) in r_iter.zip(g_iter.zip(b_iter)).cycle().step_by(50) {
-			todo!("figure out how to change config from within here properly");
-			// let r_config = self.r.config;
-			// self.r.set_config(r_lvl);
-			defmt::info!("r_count = {}", r_lvl);
-			// let g_config = self.g.config;
-			// self.g.set_config(g_lvl);
-			defmt::info!("g_count = {}", g_lvl);
-			// let b_config = self.b.config;
-			// self.b.set_config(b_lvl);
-			defmt::info!("b_count = {}", b_lvl);
-			Timer::after(Duration::from_millis(5)).await
-		}
-	}
+    #[allow(unused)]
+    async fn cycle_counter(&mut self) {
+        let fade_up = 0..=PWM_MAX;
+        let r_iter = fade_up
+            .clone()
+            .chain(core::iter::repeat_n(*fade_up.end(), fade_up.len() * 2))
+            .chain(fade_up.clone().rev())
+            .chain(core::iter::repeat_n(*fade_up.start(), fade_up.len()))
+            .cycle();
+        let g_iter = r_iter.clone().skip(fade_up.len());
+        let b_iter = g_iter.clone().skip(fade_up.len());
+        for (r_lvl, (g_lvl, b_lvl)) in r_iter.zip(g_iter.zip(b_iter)).step_by(200) {
+            let mut slice_1 = Config::default();
+            slice_1.compare_a = r_lvl;
+            slice_1.compare_b = g_lvl;
+            self.r.set_config(&slice_1);
+            // pin for g is already set
+
+            self.b.set_config(&{
+                let mut c = Config::default();
+                c.compare_a = b_lvl;
+                c
+            });
+            defmt::info!(
+                "r = {}, g = {}, b = {}",
+                r_lvl as f32 / PWM_MAX as f32,
+                g_lvl as f32 / PWM_MAX as f32,
+                b_lvl as f32 / PWM_MAX as f32
+            );
+
+            // next step: use fixed point trigonometry to make it a (co)sine wave?
+
+            Timer::after(Duration::from_millis(15)).await
+        }
+    }
 }
