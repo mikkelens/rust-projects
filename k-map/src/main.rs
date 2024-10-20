f4e855ccb87d7488bf99564ed1c2cabc1fb7566f
Mikkel <Mikkel123sej@gmail.com>
2024-10-20 23:13:58 +0200

    k-map: reinterpret formatting of Map4

--- k-map/src/main.rs
+++ k-map/src/main.rs
@@ -23,30 +23,47 @@ impl Display for MapField {
         )
     }
 }
-struct Map4([[MapField; 4]; 4]);
+struct Map4([MapField; 2usize.pow(4)]);
 
 impl Display for Map4 {
+    #[allow(clippy::erasing_op, clippy::identity_op)]
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "| AB\\CD | 00 | 01 | 11 | 10 |")?;
-        write!(
+        writeln!(f, "| AB\\CD | 00 | 01 | 11 | 10 |")?;
+        writeln!(
             f,
-            "|    00 | {} | {} | {} | {} |",
-            self.0[0][0], self.0[0][1], self.0[0][2], self.0[0][3]
+            "|    {} | {} | {} | {} | {} |",
+            "00",
+            self.0[4 * 0 + 0],
+            self.0[4 * 0 + 1],
+            self.0[4 * 0 + 2],
+            self.0[4 * 0 + 3]
         )?;
-        write!(
+        writeln!(
             f,
-            "|    01 | {} | {} | {} | {} |",
-            self.0[1][0], self.0[1][1], self.0[1][2], self.0[1][3]
+            "|    {} | {} | {} | {} | {} |",
+            "01",
+            self.0[4 * 1 + 0],
+            self.0[4 * 1 + 1],
+            self.0[4 * 1 + 2],
+            self.0[4 * 1 + 3]
         )?;
-        write!(
+        writeln!(
             f,
-            "|    11 | {} | {} | {} | {} |",
-            self.0[2][0], self.0[2][1], self.0[2][2], self.0[2][3]
+            "|    {} | {} | {} | {} | {} |",
+            "11",
+            self.0[4 * 2 + 0],
+            self.0[4 * 2 + 1],
+            self.0[4 * 2 + 2],
+            self.0[4 * 2 + 3]
         )?;
-        write!(
+        writeln!(
             f,
-            "|    10 | {} | {} | {} | {} |",
-            self.0[3][0], self.0[3][1], self.0[3][2], self.0[3][3]
+            "|    {} | {} | {} | {} | {} |",
+            "10",
+            self.0[4 * 3 + 0],
+            self.0[4 * 3 + 1],
+            self.0[4 * 3 + 2],
+            self.0[4 * 3 + 3]
         )?;
         Ok(())
     }
@@ -74,6 +91,7 @@ impl FromStr for TermExpression {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut iter = s.split('+').peekable();
+        todo!()
     }
 }
 
