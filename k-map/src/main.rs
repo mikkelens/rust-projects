b4bc51937fb58a48ec6f7d0db6ad82832a30d038
Mikkel <Mikkel123sej@gmail.com>
2024-10-30 02:23:56 +0100

    rename

--- k-map/src/main.rs
+++ k-map/src/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use std::num::NonZero;
 use std::{
     env::args,
     fmt::{Display, Formatter},
@@ -5,65 +7,71 @@ use std::{
 };
 
 #[derive(Debug)]
-enum MapField {
+enum Field {
     One,
     Zero,
-    X,
+    DontCare,
 }
-impl Display for MapField {
+impl Display for Field {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{}",
             match self {
-                MapField::One => '1',
-                MapField::Zero => '0',
-                MapField::X => 'X',
+                Field::One => '1',
+                Field::Zero => '0',
+                Field::DontCare => 'X',
             }
         )
     }
 }
-struct Map4([MapField; 2usize.pow(4)]);
+struct Map4 {
+    horizontal_slice: [Field; 2usize.pow(4)],
+}
 
 impl Display for Map4 {
     #[allow(clippy::erasing_op, clippy::identity_op)]
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "| AB\\CD | 00 | 01 | 11 | 10 |")?;
         writeln!(
             f,
-            "|    {} | {} | {} | {} | {} |",
-            "00",
-            self.0[4 * 0 + 0],
-            self.0[4 * 0 + 1],
-            self.0[4 * 0 + 2],
-            self.0[4 * 0 + 3]
+            "| AB\\CD | {:2b} | {:2b} | {:2b} | {:2b} |",
+            0b00, 0b10, 0b10, 0b10
         )?;
         writeln!(
             f,
-            "|    {} | {} | {} | {} | {} |",
-            "01",
-            self.0[4 * 1 + 0],
-            self.0[4 * 1 + 1],
-            self.0[4 * 1 + 2],
-            self.0[4 * 1 + 3]
+            "|    {:2b} |  {} |   {} |   {} |  {} |",
+            0b00,
+            self.horizontal_slice[4 * 0 + 0],
+            self.horizontal_slice[4 * 0 + 1],
+            self.horizontal_slice[4 * 0 + 2],
+            self.horizontal_slice[4 * 0 + 3]
         )?;
         writeln!(
             f,
-            "|    {} | {} | {} | {} | {} |",
-            "11",
-            self.0[4 * 2 + 0],
-            self.0[4 * 2 + 1],
-            self.0[4 * 2 + 2],
-            self.0[4 * 2 + 3]
+            "|    {:2b} |  {} |  {} |  {} |  {} |",
+            0b01,
+            self.horizontal_slice[4 * 1 + 0],
+            self.horizontal_slice[4 * 1 + 1],
+            self.horizontal_slice[4 * 1 + 2],
+            self.horizontal_slice[4 * 1 + 3]
         )?;
         writeln!(
             f,
-            "|    {} | {} | {} | {} | {} |",
-            "10",
-            self.0[4 * 3 + 0],
-            self.0[4 * 3 + 1],
-            self.0[4 * 3 + 2],
-            self.0[4 * 3 + 3]
+            "|    {:2b} |  {} |  {} |  {} |  {} |",
+            0b11,
+            self.horizontal_slice[4 * 2 + 0],
+            self.horizontal_slice[4 * 2 + 1],
+            self.horizontal_slice[4 * 2 + 2],
+            self.horizontal_slice[4 * 2 + 3]
+        )?;
+        writeln!(
+            f,
+            "|    {:2b} |  {} |  {} |  {} |  {} |",
+            0b10,
+            self.horizontal_slice[4 * 3 + 0],
+            self.horizontal_slice[4 * 3 + 1],
+            self.horizontal_slice[4 * 3 + 2],
+            self.horizontal_slice[4 * 3 + 3]
         )?;
         Ok(())
     }
